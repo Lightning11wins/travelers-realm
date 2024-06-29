886081d26d227522f437b7c95b75cada566f7d85
Lightning11wins <Lightning11wins+github@gmail.com>
2024-06-29 08:27:11 -0500

    Init

---
+++ src/main/java/net/lightning/genshinsmp/GenshinSMP.java
@@ -0,0 +1,16 @@
+package net.lightning.genshinsmp;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class GenshinSMP implements ModInitializer {
+	public static final String MOD_ID = "genshinsmp";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+		LOGGER.info("Mod initialized!");
+	}
+}
---
+++ src/main/java/net/lightning/genshinsmp/GenshinSMPDataGenerator.java
@@ -0,0 +1,11 @@
+package net.lightning.genshinsmp;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class GenshinSMPDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
