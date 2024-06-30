431f532217073f98268b417d8883015d57497603
Lightning11wins <Lightning11wins+github@gmail.com>
2024-06-30 18:46:53 -0500

    Add the HenSkull item.

--- src/main/java/net/lightning/genshinsmp/GenshinSMP.java
+++ src/main/java/net/lightning/genshinsmp/GenshinSMP.java
@@ -2,6 +2,18 @@ package net.lightning.genshinsmp;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroupEntries;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.effect.StatusEffects;
+import net.minecraft.item.FoodComponent;
+import net.minecraft.item.FoodComponents;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -12,5 +24,25 @@ public class GenshinSMP implements ModInitializer {
 	@Override
 	public void onInitialize() {
 		LOGGER.info("Mod initialized!");
+
+		Item.Settings henskullSettings = new FabricItemSettings();
+		henskullSettings.food(new FoodComponent.Builder()
+			.hunger(3)
+			.saturationModifier(1.2f)
+			.snack()
+			.alwaysEdible()
+			.statusEffect(new StatusEffectInstance(StatusEffects.BLINDNESS, 200), 1)
+			.build()
+		);
+
+		Item henskull = registerItem("henskull", new Item(henskullSettings));
+
+		ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register((FabricItemGroupEntries entries) -> {
+			entries.add(henskull);
+		});
+	}
+
+	public static Item registerItem(String name, Item item) {
+		return Registry.register(Registries.ITEM, new Identifier(MOD_ID, name), item);
 	}
 }
