467cef789c3c6e383df2ce67eff20cc410a3cef6
Lightning11wins <Lightning11wins+github@gmail.com>
2025-01-05 18:09:44 -0600

    Improve spacing of item and block declarations.

--- src/main/java/net/lightning/trealm/TravelersRealmDataGenerator.java
+++ src/main/java/net/lightning/trealm/TravelersRealmDataGenerator.java
@@ -57,88 +57,196 @@ public class TravelersRealmDataGenerator implements DataGeneratorEntrypoint {
         VISION_ITEMS = List.of(VisionItemRegistration.init());
     protected static final List<BlockRegistration> BLOCKS = new ArrayList<>();
 
-    public static final ItemRegistration HENSKULL = new ItemRegistration.Builder(new FabricItemSettings().food(new FoodComponent.Builder()
-        .hunger(3)
-        .saturationModifier(0.4f)
-        .snack()
-        .alwaysEdible()
-        .statusEffect(new StatusEffectInstance(StatusEffects.BLINDNESS, 200), 1)
+    public static final ItemRegistration HENSKULL = new ItemRegistration.Builder(
+        new FabricItemSettings().food(new FoodComponent.Builder()
+            .hunger(3)
+            .saturationModifier(0.4f)
+            .alwaysEdible()
+            .snack()
+            .statusEffect(new StatusEffectInstance(StatusEffects.BLINDNESS, 200), 1)
+            .build()
+        ))
+        .identifier("henskull")
+        .texture("item/food/henskull")
+        .displayName("HenSkull")
         .build()
-    )).identifier("henskull").texture("item/food/henskull").displayName("HenSkull").build().addTo(FOOD_ITEMS);
-    public static final ItemRegistration DISH_FURINA = new ItemRegistration.Builder(new FabricItemSettings().food(new FoodComponent.Builder()
-        .hunger(6)
-        .saturationModifier(1f)
-        .statusEffect(new StatusEffectInstance(StatusEffects.ABSORPTION, 200), 1)
+        .addTo(FOOD_ITEMS);
+    public static final ItemRegistration DISH_FURINA = new ItemRegistration.Builder(
+        new FabricItemSettings().food(new FoodComponent.Builder()
+            .hunger(6)
+            .saturationModifier(1f)
+            .statusEffect(new StatusEffectInstance(StatusEffects.ABSORPTION, 200), 1)
+            .build()
+        ))
+        .identifier("dish_furina")
+        .texture("item/food/dish_furina")
+        .displayName("Pour la Justice")
         .build()
-    )).identifier("dish_furina").texture("item/food/dish_furina").displayName("Pour la Justice").build().addTo(FOOD_ITEMS);
-    public static final ItemRegistration DISH_NAHIDA = new ItemRegistration.Builder(new FabricItemSettings().food(new FoodComponent.Builder()
-        .hunger(6)
-        .saturationModifier(1f)
-        .statusEffect(new StatusEffectInstance(StatusEffects.RESISTANCE, 200), 1)
+        .addTo(FOOD_ITEMS);
+    public static final ItemRegistration DISH_NAHIDA = new ItemRegistration.Builder(
+        new FabricItemSettings().food(new FoodComponent.Builder()
+            .hunger(6)
+            .saturationModifier(1f)
+            .statusEffect(new StatusEffectInstance(StatusEffects.RESISTANCE, 200), 1)
+            .build()
+        ))
+        .identifier("dish_nahida")
+        .texture("item/food/dish_nahida")
+        .displayName("Halvamazd")
         .build()
-    )).identifier("dish_nahida").texture("item/food/dish_nahida").displayName("Halvamazd").build().addTo(FOOD_ITEMS);
-    public static final ItemRegistration DISH_AYAKA = new ItemRegistration.Builder(new FabricItemSettings().food(new FoodComponent.Builder()
-        .hunger(6)
-        .saturationModifier(1f)
-        .statusEffect(new StatusEffectInstance(StatusEffects.INSTANT_HEALTH, 1), 1)
+        .addTo(FOOD_ITEMS);
+    public static final ItemRegistration DISH_AYAKA = new ItemRegistration.Builder(
+        new FabricItemSettings().food(new FoodComponent.Builder()
+            .hunger(6)
+            .saturationModifier(1f)
+            .statusEffect(new StatusEffectInstance(StatusEffects.INSTANT_HEALTH, 1), 1)
+            .build()
+        ))
+        .identifier("dish_ayaka")
+        .texture("item/food/dish_ayaka")
+        .displayName("Snow on the Hearth")
         .build()
-    )).identifier("dish_ayaka").texture("item/food/dish_ayaka").displayName("Snow on the Hearth").build().addTo(FOOD_ITEMS);
-    public static final ItemRegistration DISH_WANDERER = new ItemRegistration.Builder(new FabricItemSettings().food(new FoodComponent.Builder()
-        .hunger(6)
-        .saturationModifier(1f)
-        .statusEffect(new StatusEffectInstance(StatusEffects.RESISTANCE, 200), 1)
+        .addTo(FOOD_ITEMS);
+    public static final ItemRegistration DISH_WANDERER = new ItemRegistration.Builder(
+        new FabricItemSettings().food(new FoodComponent.Builder()
+            .hunger(6)
+            .saturationModifier(1f)
+            .statusEffect(new StatusEffectInstance(StatusEffects.RESISTANCE, 200), 1)
+            .build()
+        ))
+        .identifier("dish_wanderer")
+        .texture("item/food/dish_wanderer")
+        .displayName("Shimi Chazuke")
         .build()
-    )).identifier("dish_wanderer").texture("item/food/dish_wanderer").displayName("Shimi Chazuke").build().addTo(FOOD_ITEMS);
-    public static final ItemRegistration DRINK_FONTA = new ItemRegistration.Builder(new FabricItemSettings().food(new FoodComponent.Builder()
-        .hunger(2)
-        .saturationModifier(1f)
-        .statusEffect(new StatusEffectInstance(StatusEffects.GLOWING, 200), 1)
+        .addTo(FOOD_ITEMS);
+    public static final ItemRegistration DRINK_FONTA = new ItemRegistration.Builder(
+        new FabricItemSettings().food(new FoodComponent.Builder()
+            .hunger(2)
+            .saturationModifier(1f)
+            .statusEffect(new StatusEffectInstance(StatusEffects.GLOWING, 200), 1)
+            .build()
+        ))
+        .identifier("drink_fonta")
+        .texture("item/food/drink_fonta")
+        .displayName("Fonta")
         .build()
-    )).identifier("drink_fonta").texture("item/food/drink_fonta").displayName("Fonta").build().addTo(FOOD_ITEMS);
-    public static final ItemRegistration DISH_GARLIC_BAGUETTE = new ItemRegistration.Builder(new FabricItemSettings().food(new FoodComponent.Builder()
-        .hunger(4)
-        .saturationModifier(1f)
-        .statusEffect(new StatusEffectInstance(StatusEffects.STRENGTH, 300), 1)
+        .addTo(FOOD_ITEMS);
+    public static final ItemRegistration DISH_GARLIC_BAGUETTE = new ItemRegistration.Builder(
+        new FabricItemSettings().food(new FoodComponent.Builder()
+            .hunger(4)
+            .saturationModifier(1f)
+            .statusEffect(new StatusEffectInstance(StatusEffects.STRENGTH, 300), 1)
+            .build()
+        ))
+        .identifier("dish_garlic_baguette")
+        .texture("item/food/dish_garlic_baguette")
+        .displayName("Garlic Baguette")
         .build()
-    )).identifier("dish_garlic_baguette").texture("item/food/dish_garlic_baguette").displayName("Garlic Baguette").build().addTo(FOOD_ITEMS);
-    public static final ItemRegistration GOLDEN_TEMPTATION = new ItemRegistration.Builder(new FabricItemSettings().food(new FoodComponent.Builder()
-        .hunger(4)
-        .snack()
-        .saturationModifier(1f)
-        .statusEffect(new StatusEffectInstance(StatusEffects.HASTE, 400,2), 1)
+        .addTo(FOOD_ITEMS);
+    public static final ItemRegistration GOLDEN_TEMPTATION = new ItemRegistration.Builder(
+        new FabricItemSettings().food(new FoodComponent.Builder()
+            .hunger(4)
+            .snack()
+            .saturationModifier(1f)
+            .statusEffect(new StatusEffectInstance(StatusEffects.HASTE, 400,2), 1)
+            .build()
+        ))
+        .identifier("golden_temptation")
+        .texture("item/food/golden_temptation")
+        .displayName("Golden Temptation")
         .build()
-    )).identifier("golden_temptation").texture("item/food/golden_temptation").displayName("Golden Temptation").build().addTo(FOOD_ITEMS);
-    public static final ItemRegistration DISH_PADISARAH_PUDDING = new ItemRegistration.Builder(new FabricItemSettings().food(new FoodComponent.Builder()
-        .hunger(3)
-        .saturationModifier(1f)
-        .statusEffect(new StatusEffectInstance(StatusEffects.HASTE, 400,1), 1)
-        .snack()
+        .addTo(FOOD_ITEMS);
+    public static final ItemRegistration DISH_PADISARAH_PUDDING = new ItemRegistration.Builder(
+        new FabricItemSettings().food(new FoodComponent.Builder()
+            .hunger(3)
+            .saturationModifier(1f)
+            .statusEffect(new StatusEffectInstance(StatusEffects.HASTE, 400,1), 1)
+            .snack()
+            .build()
+        ))
+        .identifier("dish_padisarah_pudding")
+        .texture("item/food/dish_padisarah_pudding")
+        .displayName("Padisarah Pudding")
         .build()
-    )).identifier("dish_padisarah_pudding").texture("item/food/dish_padisarah_pudding").displayName("Padisarah Pudding").build().addTo(FOOD_ITEMS);
-    public static final ItemRegistration DRINK_RAINBOW_ASTER = new ItemRegistration.Builder(new FabricItemSettings().food(new FoodComponent.Builder()
-        .hunger(3)
-        .saturationModifier(1f)
-        .statusEffect(new StatusEffectInstance(StatusEffects.SPEED, 400,1), 1)
+        .addTo(FOOD_ITEMS);
+    public static final ItemRegistration DRINK_RAINBOW_ASTER = new ItemRegistration.Builder(
+        new FabricItemSettings().food(new FoodComponent.Builder()
+            .hunger(3)
+            .saturationModifier(1f)
+            .statusEffect(new StatusEffectInstance(StatusEffects.SPEED, 400,1), 1)
+            .build()
+        ))
+        .identifier("drink_rainbow_aster")
+        .texture("item/food/drink_rainbow_aster")
+        .displayName("Rainbow Aster")
         .build()
-    )).identifier("drink_rainbow_aster").texture("item/food/drink_rainbow_aster").displayName("Rainbow Aster").build().addTo(FOOD_ITEMS);
-    public static final ItemRegistration DISH_SHAWARMA_WRAP = new ItemRegistration.Builder(new FabricItemSettings().food(new FoodComponent.Builder()
-        .hunger(6)
-        .saturationModifier(1f)
-        .statusEffect(new StatusEffectInstance(StatusEffects.STRENGTH, 200), 1)
+        .addTo(FOOD_ITEMS);
+    public static final ItemRegistration DISH_SHAWARMA_WRAP = new ItemRegistration.Builder(
+        new FabricItemSettings().food(new FoodComponent.Builder()
+            .hunger(6)
+            .saturationModifier(1f)
+            .statusEffect(new StatusEffectInstance(StatusEffects.STRENGTH, 200), 1)
+            .build()
+        ))
+        .identifier("dish_shawarma_wrap")
+        .texture("item/food/dish_shawarma_wrap")
+        .displayName("Shawarma Wrap")
         .build()
-    )).identifier("dish_shawarma_wrap").texture("item/food/dish_shawarma_wrap").displayName("Shawarma Wrap").build().addTo(FOOD_ITEMS);
+        .addTo(FOOD_ITEMS);
 
-    public static final ItemRegistration ALMOND = new ItemRegistration.Builder().identifier("almond").texture("item/food/almond").displayName("Almonds").build().addTo(INGREDIENT_ITEMS);
-    public static final ItemRegistration CRAB = new ItemRegistration.Builder().identifier("crab").texture("item/food/crab").displayName("Crab").build().addTo(INGREDIENT_ITEMS);
-    public static final ItemRegistration SHRIMP = new ItemRegistration.Builder().identifier("shrimp").texture("item/food/shrimp").displayName("Shrimp").build().addTo(INGREDIENT_ITEMS);
+    public static final ItemRegistration ALMOND = new ItemRegistration.Builder()
+        .identifier("almond")
+        .texture("item/food/almond")
+        .displayName("Almonds")
+        .build()
+        .addTo(INGREDIENT_ITEMS);
+    public static final ItemRegistration CRAB = new ItemRegistration.Builder()
+        .identifier("crab")
+        .texture("item/food/crab")
+        .displayName("Crab")
+        .build()
+        .addTo(INGREDIENT_ITEMS);
+    public static final ItemRegistration SHRIMP = new ItemRegistration.Builder()
+        .identifier("shrimp")
+        .texture("item/food/shrimp")
+        .displayName("Shrimp")
+        .build()
+        .addTo(INGREDIENT_ITEMS);
 
-    public static final ItemRegistration ELEMENTAL_INGOT = new ItemRegistration.Builder().identifier("elemental_ingot").texture("item/elemental_ingot").displayName("Elemental Ingot").build().addTo(INGREDIENT_ITEMS);
-    public static final ItemRegistration ELEMENTAL_NUGGET = new ItemRegistration.Builder().identifier("elemental_nugget").texture("item/elemental_nugget").displayName("Elemental Nugget").build().addTo(INGREDIENT_ITEMS);
-    public static final ItemRegistration RAW_ELEMENTAL_ORE = new ItemRegistration.Builder().identifier("raw_elemental_ore").texture("item/raw_elemental_ore").displayName("Raw Elemental Ore").build().addTo(INGREDIENT_ITEMS);
+    public static final ItemRegistration ELEMENTAL_INGOT = new ItemRegistration.Builder()
+        .identifier("elemental_ingot")
+        .texture("item/elemental_ingot")
+        .displayName("Elemental Ingot")
+        .build()
+        .addTo(INGREDIENT_ITEMS);
+    public static final ItemRegistration ELEMENTAL_NUGGET = new ItemRegistration.Builder()
+        .identifier("elemental_nugget")
+        .texture("item/elemental_nugget")
+        .displayName("Elemental Nugget")
+        .build()
+        .addTo(INGREDIENT_ITEMS);
+    public static final ItemRegistration RAW_ELEMENTAL_ORE = new ItemRegistration.Builder()
+        .identifier("raw_elemental_ore")
+        .texture("item/raw_elemental_ore")
+        .displayName("Raw Elemental Ore")
+        .build()
+        .addTo(INGREDIENT_ITEMS);
 
-    public static final BlockRegistration DEEPSLATE_ELEMENTAL_ORE = new BlockRegistration.Builder(Blocks.DEEPSLATE_DIAMOND_ORE).identifier("deepslate_elemental_ore").texture("block/elemental_ore").displayName("Elemental Ore").build();
-    public static final BlockRegistration ELEMENTAL_BLOCK = new BlockRegistration.Builder(Blocks.DIAMOND_BLOCK).identifier("elemental_block").texture("block/elemental_block").displayName("Elemental Block").build();
-    public static final BlockRegistration RAW_ELEMENTAL_BLOCK = new BlockRegistration.Builder(Blocks.RAW_IRON_BLOCK).identifier("raw_elemental_block").texture("block/raw_elemental_block").displayName("Block of Raw Elemental Ore").build();
+    public static final BlockRegistration DEEPSLATE_ELEMENTAL_ORE = new BlockRegistration.Builder(Blocks.DEEPSLATE_DIAMOND_ORE)
+        .identifier("deepslate_elemental_ore")
+        .texture("block/elemental_ore")
+        .displayName("Elemental Ore")
+        .build();
+    public static final BlockRegistration ELEMENTAL_BLOCK = new BlockRegistration.Builder(Blocks.DIAMOND_BLOCK)
+        .identifier("elemental_block")
+        .texture("block/elemental_block")
+        .displayName("Elemental Block")
+        .build();
+    public static final BlockRegistration RAW_ELEMENTAL_BLOCK = new BlockRegistration.Builder(Blocks.RAW_IRON_BLOCK)
+        .identifier("raw_elemental_block")
+        .texture("block/raw_elemental_block")
+        .displayName("Block of Raw Elemental Ore")
+        .build();
 
     @Override
 	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
